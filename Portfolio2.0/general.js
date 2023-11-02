139695d43f3d145a38a6793cc539fc9e66b27b7c
Hage <89595749+HageFX-78@users.noreply.github.com>
2023-11-02 18:18:47 +0800

    Link style [a] organized with divs

--- Portfolio2.0/general.js
+++ Portfolio2.0/general.js
@@ -7,4 +7,28 @@ $(document).ready(function() {
     $("#fumo").click(function() {
         $(".modal-box-back").show();
     });
+
+
+    // $(document).keypress(function (event) {
+ 
+
+    // });
 });
+
+
+// Horizontal scrolling on screenshots
+$(document).on('wheel', '.ele-container2', function (e) {
+    var $scrollArea = $('.ele-container2');
+    if ($scrollArea.get(0).scrollWidth <= $scrollArea.get(0).clientWidth) {
+        // No need to scroll horizontally if there's no overflow
+        return;
+    }
+
+    var scrollAmount = e.originalEvent.deltaY;
+
+    $('.ele-container2').on('wheel', function (e) {
+        e.preventDefault();
+        $('.ele-container2').scrollLeft($('.ele-container2').scrollLeft() + scrollAmount);
+    });
+});
+
