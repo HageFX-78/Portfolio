433eb2e4dae65fa53dcee83861690ede47729934
Hage <89595749+HageFX-78@users.noreply.github.com>
2025-06-22 16:15:25 +0800

    Minor animation refactor

--- public/_app/immutable/chunks/DGv1U5o1.js
+++
@@ -1,6 +0,0 @@
-const e=`        Alex:Um do you know where is this?\r
-        ???:Not really\r
-        ???:Why don't you try asking that guy over there..:puzzleGuy\r
-        ???:Dudes been rambling about some kind of puzzle the whole time\r
-        ???:Might be useful you know hehe..\r
-        Alex:Uh.. ok thanks?...:player`;export{e as default};
